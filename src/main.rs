40eaafd62f57d7a53dc5c94800ef24c9999d22fd
Stephen Buchanan <stephen.buchanan@random.studio>
2023-02-10 11:29:32 +0100

    very basic single animation works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,59 @@
 use nannou::prelude::*;
+
+pub trait Animated {
+    fn duration(&self) -> u128;
+    fn start_time(&self) -> u128;
+
+    /// Update the animation with the current time, get the progress in the range `[0,1]`
+    fn update(&self, current_time: u128) -> f32 {
+        let elapsed = current_time - self.start_time();
+        let progress = elapsed.to_f64().unwrap() / self.duration().to_f64().unwrap();
+        progress.to_f32().unwrap()
+    }
+}
+
+#[derive(Debug)]
 struct Particle {
     position: Point2,
-    brightness: f32
+    brightness: f32,
+    animation: EnvelopeStage,
+}
+
+#[derive(Debug)]
+struct Attack {
+    start_time: u128,
+    duration: u128,
+}
+
+impl Animated for Attack {
+    fn start_time(&self) -> u128 {
+        self.start_time
+    }
+    fn duration(&self) -> u128 {
+        self.duration
+    }
+}
+#[derive(Debug)]
+struct Release {
+    start_time: u128,
+    duration: u128,
+}
+
+impl Animated for Release {
+    fn start_time(&self) -> u128 {
+        self.start_time
+    }
+    fn duration(&self) -> u128 {
+        self.duration
+    }
+}
+
+// The animation concept is based on https://en.wikipedia.org/wiki/Envelope_(music)
+#[derive(Debug)]
+enum EnvelopeStage {
+    AttackAnimation(Attack),
+    ReleaseAnimation(Release),
+    Idle(),
 }
 
 const THICKNESS: f32 = 5.;
@@ -13,29 +65,52 @@ fn main() {
 
 struct Model {
     _window: window::Id,
-    particles: Vec<Particle>
+    particles: Vec<Particle>,
 }
 
 fn model(app: &App) -> Model {
     let _window = app.new_window().view(view).build().unwrap();
-    Model { _window, particles: vec![
-        Particle{
-            position: Vec2::new(0., 0.),
-            brightness: 1.0
-        },
-        Particle{
-            position: Vec2::new(100., 100.),
-            brightness: 0.5
-        },
-        Particle{
-            position: Vec2::new(-100., 50.),
-            brightness: 0.5
-        }
-
-    ] }
+    Model {
+        _window,
+        particles: vec![
+            Particle {
+                position: Vec2::new(0., 0.),
+                brightness: 1.0,
+                animation: EnvelopeStage::Idle(),
+            },
+            Particle {
+                position: Vec2::new(100., 100.),
+                brightness: 0.5,
+                animation: EnvelopeStage::Idle(),
+            },
+            Particle {
+                position: Vec2::new(-100., 50.),
+                brightness: 0.,
+                animation: EnvelopeStage::AttackAnimation(Attack {
+                    start_time: app.duration.since_prev_update.as_millis(),
+                    duration: 5000,
+                }),
+            },
+        ],
+    }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+fn update(app: &App, model: &mut Model, _update: Update) {
+    for p in &mut model.particles {
+        let animation = &mut p.animation;
+        // let current_time = app.duration.since_prev_update.as_millis();
+        let current_time = app.duration.since_start.as_millis();
+        println!("current time {}", current_time);
+        if let Some(progress) = match animation {
+            EnvelopeStage::AttackAnimation(a) => Some(a.update(current_time)),
+            EnvelopeStage::ReleaseAnimation(a) => Some(a.update(current_time)),
+            EnvelopeStage::Idle() => None,
+        } {
+            println!("updating {:?} with progress {}", p, progress);
+            p.brightness = if progress > 1. { 1. } else { progress };
+        }
+    }
+}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
@@ -43,13 +118,12 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     for p in &model.particles {
         draw
-        // .ellipse()
-        .rect()
-        .w_h(THICKNESS, LENGTH)
-        .x_y(p.position.x, p.position.y)
-        .color(gray(p.brightness));
+            // .ellipse()
+            .rect()
+            .w_h(THICKNESS, LENGTH)
+            .x_y(p.position.x, p.position.y)
+            .color(gray(p.brightness));
     }
 
     draw.to_frame(app, &frame).unwrap();
 }
-
