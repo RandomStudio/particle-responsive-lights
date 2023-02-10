f7396c73b6c50ad2c1f79d8c055d39c8c3b920ce
Stephen Buchanan <stephen.buchanan@random.studio>
2023-02-10 09:36:44 +0100

    drawing chimes as rects

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ struct Particle {
     brightness: f32
 }
 
+const THICKNESS: f32 = 5.;
+const LENGTH: f32 = 100.;
+
 fn main() {
     nannou::app(model).update(update).run();
 }
@@ -22,7 +25,11 @@ fn model(app: &App) -> Model {
         },
         Particle{
             position: Vec2::new(100., 100.),
-            brightness: 1.0
+            brightness: 0.5
+        },
+        Particle{
+            position: Vec2::new(-100., 50.),
+            brightness: 0.5
         }
 
     ] }
@@ -32,12 +39,15 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(PLUM);
+    draw.background().color(DARKSLATEGREY);
 
     for p in &model.particles {
-        draw.ellipse()
+        draw
+        // .ellipse()
+        .rect()
+        .w_h(THICKNESS, LENGTH)
         .x_y(p.position.x, p.position.y)
-        .color(STEELBLUE);
+        .color(gray(p.brightness));
     }
 
     draw.to_frame(app, &frame).unwrap();
